f917b49feef6b5aafaf9a459ecfafc15dc8e2b09
Sandee007 <sandeepaalahakoon@gmail.com>
2020-11-29 12:33:28 +0530

    sdfdfdf

---
+++ database/migrations/2020_11_29_043325_create_member_creation_nominees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberCreationNomineesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_creation_nominees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('member_id')->nullable();
+            $table->string('nominee_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_creation_nominees');
+    }
+}
