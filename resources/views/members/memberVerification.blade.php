3e0c7778b6f6bb33932998ef9e788e9e2ddef3c4
indunillakshitha <skgindunil@gmail.com>
2020-12-07 02:59:56 +0530

    member verification to fix

--- resources/views/members/memberVerification.blade.php
+++ resources/views/members/memberVerification.blade.php
@@ -33,7 +33,8 @@
                     <label class="col-sm-2 col-form-label">>ID Number</label>
                     <div class="col-sm-10">
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Enter Identification No" name="identification_number" id="identification_number">
+                            <input type="text" class="form-control" placeholder="Enter Identification No"
+                                name="identification_number" id="identification_number">
                             <span class="bmd-help">Use Client ID Number To Search</span>
                         </div>
                     </div>
@@ -72,22 +73,22 @@
                                 <tbody id="results_tbody">
                                     @isset($membrs)
                                     @foreach ($membrs as $membr)
-                                <tr>
-                                    <th>{{$membr->customer_id }}</th>
-                                    <th>{{$membr->name_in_use }}</th>
-                                    <th>{{$membr->full_name }}</th>
-                                    <th>{{$membr->identification_number }}</th>
-                                    <td class="td-actions text-right">
-                                        <a href="{{url('/members/view/check/'.$membr->customer_id)}}" rel="tooltip"
-                                            class="btn btn-info btn-round">
-                                            <i class="material-icons">edit</i> <span class="mx-1">Check</span>
-                                        </a>
+                                    <tr>
+                                        <th>{{$membr->customer_id }}</th>
+                                        <th>{{$membr->name_in_use }}</th>
+                                        <th>{{$membr->full_name }}</th>
+                                        <th>{{$membr->identification_number }}</th>
+                                        <td class="td-actions text-right">
+                                            <a href="{{url('/members/view/check/'.$membr->customer_id)}}" rel="tooltip"
+                                                class="btn btn-info btn-round">
+                                                <i class="material-icons">edit</i> <span class="mx-1">Check</span>
+                                            </a>
 
-                                        <a href="{{url('/members/view/verify/'.$membr->customer_id)}}" rel="tooltip"
-                                            class="btn btn-primary btn-round">
-                                            <i class="material-icons">check</i> <span class="mx-1">Verify</span>
-                                        </a>
-                                    </td>
+                                            <a href="{{url('/members/view/verify/'.$membr->customer_id)}}" rel="tooltip"
+                                                class="btn btn-primary btn-round">
+                                                <i class="material-icons">check</i> <span class="mx-1">Verify</span>
+                                            </a>
+                                        </td>
 
                                     </tr>
                                     @endforeach
@@ -106,53 +107,53 @@
 
 <script>
     function search(){
-        let customer_id = $("#customer_id").val();
-        let identification_number = $("#identification_number").val();
-        let full_name = $("#full_name").val();
-        let religion_data_id = $("#religion_data_id").val();
-        let gender_id = $("#gender_id").val();
-        let married_status_id = $("#married_status_id").val();
-        let expire_date = $("#expire_date").val();
-        let join_date = $("#join_date").val();
-        //alert(religion_data_id);
-        $.ajax({
-            type: 'POST',
-            url: '{{('/members/search')}}',
-            data: {
-                "customer_id": customer_id,
-                "identification_number": identification_number,
-                "full_name": full_name,
-                "religion_data_id": religion_data_id,
-                "gender_id": gender_id,
-                "married_status_id": married_status_id,
-                "expire_date": expire_date,
-                "join_date": join_date,
-                "for_verify": 1,
-            },
-        }).done(function(data) {
-            //console.log(data);
-            return show_data(data);
-        });
-    }
+let customer_id = $("#customer_id").val();
+let identification_number = $("#identification_number").val();
+let full_name = $("#full_name").val();
+let religion_data_id = $("#religion_data_id").val();
+let gender_id = $("#gender_id").val();
+let married_status_id = $("#married_status_id").val();
+let expire_date = $("#expire_date").val();
+let join_date = $("#join_date").val();
+//alert(religion_data_id);
+$.ajax({
+type: 'POST',
+url: '{{('/members/search')}}',
+data: {
+"customer_id": customer_id,
+"identification_number": identification_number,
+"full_name": full_name,
+"religion_data_id": religion_data_id,
+"gender_id": gender_id,
+"married_status_id": married_status_id,
+"expire_date": expire_date,
+"join_date": join_date,
+"for_verify": 1,
+},
+}).done(function(data) {
+//console.log(data);
+return show_data(data);
+});
+}
 
-    function show_data(data){
-        results_tbody.innerHTML = ''
-        data.forEach(i => {
-            html = `
-            <tr>
-                <th>${i.customer_id} </th>
-                <th>${i.identification_number} </th>
-                <th>${i.full_name}</th>
-                <th>${i.status}</th>
-                <th>
-                    <a href="/members/view/check/${i.customer_id}" class="btn btn-primary" >Check</a>
-                    <a href="/members/view/verify/${i.customer_id}" class="btn btn-primary" >Verify</a>
-                </th>
-            </tr>
-            `
-            results_tbody.innerHTML += html
-        })
-    }
+function show_data(data){
+results_tbody.innerHTML = ''
+data.forEach(i => {
+html = `
+<tr>
+<th>${i.customer_id} </th>
+<th>${i.identification_number} </th>
+<th>${i.full_name}</th>
+<th>${i.status}</th>
+<th>
+<a href="/members/view/check/${i.customer_id}" class="btn btn-primary" >Check</a>
+<a href="/members/view/verify/${i.customer_id}" class="btn btn-primary" >Verify</a>
+</th>
+</tr>
+`
+results_tbody.innerHTML += html
+})
+}
 </script>
 
 @endsection
