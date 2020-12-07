7555d4000652d0dd0c8865097f1c4d267958daf5
DESKTOP-08RF9RG\Lahiru Chamika <chamika@lpw.lk>
2020-12-07 22:50:41 +0530

    Fixed data issue in customer search

--- app/Http/Controllers/OpenSavingsAccountController.php
+++ app/Http/Controllers/OpenSavingsAccountController.php
@@ -111,7 +111,7 @@ class OpenSavingsAccountController extends Controller
             csd.date_of_birth, b.branch_code, m.share_amount, agi.account_balance, agi.account_number
         FROM customer_basic_data AS cbd
         INNER JOIN branches AS b ON b.id = cbd.branch_id
-        INNER JOIN account_general_information AS agi ON agi.customer_id = cbd.customer_id
+        LEFT JOIN account_general_information AS agi ON agi.customer_id = cbd.customer_id
         INNER JOIN customer_status_dates AS csd ON csd.customer_id = cbd.customer_id
         LEFT JOIN members AS m ON m.customer_id = cbd.customer_id
         WHERE cbd.full_name LIKE '%$request->text%' AND cbd.is_enable = 1 AND cbd.status != 3";
